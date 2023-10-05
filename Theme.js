fa4fa1e1d8e1988663d477f2ad286bf9703bb1ef
Nolvos <119397000+Nolvos@users.noreply.github.com>
2023-10-05 15:35:02 +0400

    Create Theme.js

---
+++ Theme.js
@@ -0,0 +1,24 @@
+// Theme.js
+
+const theme = {
+  colors: {
+    primary: '#007BFF',
+    secondary: '#6C757D',
+    success: '#28A745',
+    danger: '#DC3545',
+    warning: '#FFC107',
+    info: '#17A2B8',
+  },
+  fonts: {
+    family: 'sans-serif',
+  },
+  breakpoints: {
+    xs: '576px',
+    sm: '768px',
+    md: '992px',
+    lg: '1200px',
+    xl: '1400px',
+  },
+};
+
+export default theme;
