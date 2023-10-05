a4446e7aef5bc3472b08f0d746e7e4787d681736
Nolvos <119397000+Nolvos@users.noreply.github.com>
2023-10-05 22:57:24 +0400

    Create result.py

---
+++ result.py
@@ -0,0 +1,53 @@
+Transcribing /content/drive/MyDrive/Animals_Dataset/training/CKUbAGwN.mp3
+Transcribing /content/drive/MyDrive/Animals_Dataset/training/6XHCJ0d9.mp3
+Transcribing /content/drive/MyDrive/Animals_Dataset/training/NbMgkZr8.mp3
+Transcribing /content/drive/MyDrive/Animals_Dataset/training/16mLRrfH.mp3
+Transcribing /content/drive/MyDrive/Animals_Dataset/training/y7dRBV0X.mp3
+Transcribing /content/drive/MyDrive/Animals_Dataset/training/WNigy84H.mp3
+Transcribing /content/drive/MyDrive/Animals_Dataset/training/eorHWiFO.mp3
+Google Web Speech API could not understand audio
+Transcription is None. Skipping.
+Transcribing /content/drive/MyDrive/Animals_Dataset/training/jh9Gwbsy.mp3
+Transcribing /content/drive/MyDrive/Animals_Dataset/training/oEx6R8NF.mp3
+Transcribing /content/drive/MyDrive/Animals_Dataset/training/q87MwIW0.mp3
+Transcribing /content/drive/MyDrive/Animals_Dataset/training/Vi8DU62C.mp3
+Transcribing /content/drive/MyDrive/Animals_Dataset/training/1TEWgS4j.mp3
+Found 101 unique tokens.
+Model: "sequential_8"
+_________________________________________________________________
+ Layer (type)                Output Shape              Param #   
+=================================================================
+ embedding_8 (Embedding)     (None, 100, 100)          1000000   
+                                                                 
+ bidirectional_8 (Bidirecti  (None, 256)               234496    
+ onal)                                                           
+                                                                 
+ dropout_8 (Dropout)         (None, 256)               0         
+                                                                 
+ dense_8 (Dense)             (None, 11)                2827      
+                                                                 
+=================================================================
+Total params: 1237323 (4.72 MB)
+Trainable params: 1237323 (4.72 MB)
+Non-trainable params: 0 (0.00 Byte)
+_________________________________________________________________
+Epoch 1/10
+1/1 [==============================] - 6s 6s/step - loss: 2.3922 - accuracy: 0.0000e+00 - val_loss: 2.4026 - val_accuracy: 0.0000e+00
+Epoch 2/10
+1/1 [==============================] - 0s 370ms/step - loss: 2.3920 - accuracy: 0.1111 - val_loss: 2.4258 - val_accuracy: 0.0000e+00
+Epoch 3/10
+1/1 [==============================] - 0s 301ms/step - loss: 2.3695 - accuracy: 0.4444 - val_loss: 2.4506 - val_accuracy: 0.0000e+00
+Epoch 4/10
+1/1 [==============================] - 0s 347ms/step - loss: 2.3538 - accuracy: 0.4444 - val_loss: 2.4816 - val_accuracy: 0.0000e+00
+Epoch 5/10
+1/1 [==============================] - 0s 328ms/step - loss: 2.3555 - accuracy: 0.1111 - val_loss: 2.5103 - val_accuracy: 0.0000e+00
+Epoch 6/10
+1/1 [==============================] - 0s 326ms/step - loss: 2.3526 - accuracy: 0.1111 - val_loss: 2.5417 - val_accuracy: 0.0000e+00
+Epoch 7/10
+1/1 [==============================] - 0s 355ms/step - loss: 2.3139 - accuracy: 0.3333 - val_loss: 2.5764 - val_accuracy: 0.0000e+00
+Epoch 8/10
+1/1 [==============================] - 0s 333ms/step - loss: 2.2916 - accuracy: 0.6667 - val_loss: 2.6184 - val_accuracy: 0.0000e+00
+Epoch 9/10
+1/1 [==============================] - 0s 315ms/step - loss: 2.2614 - accuracy: 0.4444 - val_loss: 2.6722 - val_accuracy: 0.0000e+00
+Epoch 10/10
+1/1 [==============================] - 0s 322ms/step - loss: 2.2886 - accuracy: 0.1111 - val_loss: 2.7310 - val_accuracy: 0.0000e+00
