cca497fdf1b85b9b5cb625b7fea323807bdecae0
Nolvos <119397000+Nolvos@users.noreply.github.com>
2023-10-05 15:47:48 +0400

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,30 @@
-// App.js
-
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import theme from './Theme';
 import App from './App';
 
-const Root = () => (
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
-);
+const Root = () => {
+  const [result, setResult] = useState('');
 
-const handleClick = async () => {
-  // Make a request to the Python function
-  const response = await fetch('http://localhost:5000/predict');
+  const handleClick = async () => {
+    // Make a request to the Python function
+    const response = await fetch('http://localhost:5000/predict');
 
-  // Get the response body
-  const body = await response.json();
+    // Get the response body
+    const body = await response.json();
 
-  // Set the result state
-  setResult(body.result);
+    // Set the result state
+    setResult(body.result);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <App />
+      <input type="text" value={result} />
+      <button onClick={handleClick}>What is ?</button>
+    </ThemeProvider>
+  );
 };
-ReactDOM.render(<Root />, document.getElementById('root'));
 
+ReactDOM.render(<Root />, document.getElementById('root'));
