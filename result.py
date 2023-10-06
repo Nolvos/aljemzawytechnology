451aca3e4f987376a04b4475bdde935b3c69c8f0
Nolvos <119397000+Nolvos@users.noreply.github.com>
2023-10-06 13:47:15 +0400

    Update result.py

--- result.py
+++ result.py
@@ -1,53 +1,100 @@
+[nltk_data] Downloading package punkt to /root/nltk_data...
+[nltk_data]   Package punkt is already up-to-date!
+[nltk_data] Downloading package averaged_perceptron_tagger to
+[nltk_data]     /root/nltk_data...
+[nltk_data]   Package averaged_perceptron_tagger is already up-to-
+[nltk_data]       date!
+Transcribing /content/drive/MyDrive/Animals_Dataset/testing/Record (online-voice-recorder (Joined by Happy Scribe).mp3
+Transcription: the lion and Arabic once there was a line in the jungle used to kill two to three animals stay for his name all animals went to him
+Sentences: ['the lion and Arabic once there was a line in the jungle used to kill two to three animals stay for his name all animals went to him']
+Linguistic analysis for sentence: the lion and Arabic once there was a line in the jungle used to kill two to three animals stay for his name all animals went to him
+Word count: 28
+Sentence count: 1
+Part-of-speech tags: [('the', 'DT'), ('lion', 'NN'), ('and', 'CC'), ('Arabic', 'NNP'), ('once', 'RB'), ('there', 'EX'), ('was', 'VBD'), ('a', 'DT'), ('line', 'NN'), ('in', 'IN'), ('the', 'DT'), ('jungle', 'NN'), ('used', 'VBN'), ('to', 'TO'), ('kill', 'VB'), ('two', 'CD'), ('to', 'TO'), ('three', 'CD'), ('animals', 'NNS'), ('stay', 'VBP'), ('for', 'IN'), ('his', 'PRP$'), ('name', 'NN'), ('all', 'DT'), ('animals', 'NNS'), ('went', 'VBD'), ('to', 'TO'), ('him', 'PRP')]
+Transcribing /content/drive/MyDrive/Animals_Dataset/testing/Record (online-voice-recorder.com) (3).mp3
+Transcription: show the lion agreed and distorted going for many days one day it was Robert Stern when he was on his way he saw a well
+Sentences: ['show the lion agreed and distorted going for many days one day it was Robert Stern when he was on his way he saw a well']
+Linguistic analysis for sentence: show the lion agreed and distorted going for many days one day it was Robert Stern when he was on his way he saw a well
+Word count: 26
+Sentence count: 1
+Part-of-speech tags: [('show', 'VB'), ('the', 'DT'), ('lion', 'NN'), ('agreed', 'VBD'), ('and', 'CC'), ('distorted', 'VBD'), ('going', 'VBG'), ('for', 'IN'), ('many', 'JJ'), ('days', 'NNS'), ('one', 'CD'), ('day', 'NN'), ('it', 'PRP'), ('was', 'VBD'), ('Robert', 'NNP'), ('Stern', 'NNP'), ('when', 'WRB'), ('he', 'PRP'), ('was', 'VBD'), ('on', 'IN'), ('his', 'PRP$'), ('way', 'NN'), ('he', 'PRP'), ('saw', 'VBD'), ('a', 'DT'), ('well', 'NN')]
 Transcribing /content/drive/MyDrive/Animals_Dataset/training/CKUbAGwN.mp3
+Transcription: The Lion and the rabbit once there was a lion in the jungle who used to kill two to three animals daily for his meal all animals went to him to tell that daily one of them will come to him for his meal
+Sentences: ['The Lion and the rabbit once there was a lion in the jungle who used to kill two to three animals daily for his meal all animals went to him to tell that daily one of them will come to him for his meal']
+Linguistic analysis for sentence: The Lion and the rabbit once there was a lion in the jungle who used to kill two to three animals daily for his meal all animals went to him to tell that daily one of them will come to him for his meal
+Word count: 44
+Sentence count: 1
+Part-of-speech tags: [('The', 'DT'), ('Lion', 'NNP'), ('and', 'CC'), ('the', 'DT'), ('rabbit', 'NN'), ('once', 'RB'), ('there', 'EX'), ('was', 'VBD'), ('a', 'DT'), ('lion', 'NN'), ('in', 'IN'), ('the', 'DT'), ('jungle', 'NN'), ('who', 'WP'), ('used', 'VBD'), ('to', 'TO'), ('kill', 'VB'), ('two', 'CD'), ('to', 'TO'), ('three', 'CD'), ('animals', 'NNS'), ('daily', 'RB'), ('for', 'IN'), ('his', 'PRP$'), ('meal', 'NN'), ('all', 'DT'), ('animals', 'NNS'), ('went', 'VBD'), ('to', 'TO'), ('him', 'PRP'), ('to', 'TO'), ('tell', 'VB'), ('that', 'IN'), ('daily', 'JJ'), ('one', 'CD'), ('of', 'IN'), ('them', 'PRP'), ('will', 'MD'), ('come', 'VB'), ('to', 'TO'), ('him', 'PRP'), ('for', 'IN'), ('his', 'PRP$'), ('meal', 'NN')]
 Transcribing /content/drive/MyDrive/Animals_Dataset/training/6XHCJ0d9.mp3
+Transcription: when he was on his way he saw well
+Sentences: ['when he was on his way he saw well']
+Linguistic analysis for sentence: when he was on his way he saw well
+Word count: 9
+Sentence count: 1
+Part-of-speech tags: [('when', 'WRB'), ('he', 'PRP'), ('was', 'VBD'), ('on', 'IN'), ('his', 'PRP$'), ('way', 'NN'), ('he', 'PRP'), ('saw', 'VBD'), ('well', 'RB')]
 Transcribing /content/drive/MyDrive/Animals_Dataset/training/NbMgkZr8.mp3
+Transcription: now he plans to kill the lion and save himself he went to the lion and told him that there is another lion who claims to be more powerful than him
+Sentences: ['now he plans to kill the lion and save himself he went to the lion and told him that there is another lion who claims to be more powerful than him']
+Linguistic analysis for sentence: now he plans to kill the lion and save himself he went to the lion and told him that there is another lion who claims to be more powerful than him
+Word count: 31
+Sentence count: 1
+Part-of-speech tags: [('now', 'RB'), ('he', 'PRP'), ('plans', 'VBZ'), ('to', 'TO'), ('kill', 'VB'), ('the', 'DT'), ('lion', 'NN'), ('and', 'CC'), ('save', 'VB'), ('himself', 'PRP'), ('he', 'PRP'), ('went', 'VBD'), ('to', 'TO'), ('the', 'DT'), ('lion', 'NN'), ('and', 'CC'), ('told', 'VBD'), ('him', 'PRP'), ('that', 'IN'), ('there', 'EX'), ('is', 'VBZ'), ('another', 'DT'), ('lion', 'NN'), ('who', 'WP'), ('claims', 'VBZ'), ('to', 'TO'), ('be', 'VB'), ('more', 'RBR'), ('powerful', 'JJ'), ('than', 'IN'), ('him', 'PRP')]
 Transcribing /content/drive/MyDrive/Animals_Dataset/training/16mLRrfH.mp3
+Transcription: and said he lives here
+Sentences: ['and said he lives here']
+Linguistic analysis for sentence: and said he lives here
+Word count: 5
+Sentence count: 1
+Part-of-speech tags: [('and', 'CC'), ('said', 'VBD'), ('he', 'PRP'), ('lives', 'VBZ'), ('here', 'RB')]
 Transcribing /content/drive/MyDrive/Animals_Dataset/training/y7dRBV0X.mp3
+Transcription: when the lion looked in the well he saw his own reflection and jumped in the well and dies
+Sentences: ['when the lion looked in the well he saw his own reflection and jumped in the well and dies']
+Linguistic analysis for sentence: when the lion looked in the well he saw his own reflection and jumped in the well and dies
+Word count: 19
+Sentence count: 1
+Part-of-speech tags: [('when', 'WRB'), ('the', 'DT'), ('lion', 'NN'), ('looked', 'VBD'), ('in', 'IN'), ('the', 'DT'), ('well', 'NN'), ('he', 'PRP'), ('saw', 'VBD'), ('his', 'PRP$'), ('own', 'JJ'), ('reflection', 'NN'), ('and', 'CC'), ('jumped', 'VBD'), ('in', 'IN'), ('the', 'DT'), ('well', 'NN'), ('and', 'CC'), ('dies', 'NNS')]
 Transcribing /content/drive/MyDrive/Animals_Dataset/training/WNigy84H.mp3
+Transcription: the Hunter and the pigeons one day a hunter sets a net to catch birds and placed brains and rice over the net
+Sentences: ['the Hunter and the pigeons one day a hunter sets a net to catch birds and placed brains and rice over the net']
+Linguistic analysis for sentence: the Hunter and the pigeons one day a hunter sets a net to catch birds and placed brains and rice over the net
+Word count: 23
+Sentence count: 1
+Part-of-speech tags: [('the', 'DT'), ('Hunter', 'NNP'), ('and', 'CC'), ('the', 'DT'), ('pigeons', 'NNS'), ('one', 'CD'), ('day', 'NN'), ('a', 'DT'), ('hunter', 'NN'), ('sets', 'VBZ'), ('a', 'DT'), ('net', 'JJ'), ('to', 'TO'), ('catch', 'VB'), ('birds', 'NNS'), ('and', 'CC'), ('placed', 'VBD'), ('brains', 'NNS'), ('and', 'CC'), ('rice', 'NN'), ('over', 'IN'), ('the', 'DT'), ('net', 'JJ')]
 Transcribing /content/drive/MyDrive/Animals_Dataset/training/eorHWiFO.mp3
 Google Web Speech API could not understand audio
-Transcription is None. Skipping.
+Transcription is None. Skipping linguistic analysis.
 Transcribing /content/drive/MyDrive/Animals_Dataset/training/jh9Gwbsy.mp3
+Transcription: they did as they were told and carried the net away
+Sentences: ['they did as they were told and carried the net away']
+Linguistic analysis for sentence: they did as they were told and carried the net away
+Word count: 11
+Sentence count: 1
+Part-of-speech tags: [('they', 'PRP'), ('did', 'VBD'), ('as', 'IN'), ('they', 'PRP'), ('were', 'VBD'), ('told', 'VBN'), ('and', 'CC'), ('carried', 'VBD'), ('the', 'DT'), ('net', 'JJ'), ('away', 'RB')]
 Transcribing /content/drive/MyDrive/Animals_Dataset/training/oEx6R8NF.mp3
+Transcription: the hunter runs after them but they flew away to their friend's household then the mouse cuts the net and freed the pigeons
+Sentences: ["the hunter runs after them but they flew away to their friend's household then the mouse cuts the net and freed the pigeons"]
+Linguistic analysis for sentence: the hunter runs after them but they flew away to their friend's household then the mouse cuts the net and freed the pigeons
+Word count: 24
+Sentence count: 1
+Part-of-speech tags: [('the', 'DT'), ('hunter', 'NN'), ('runs', 'VBZ'), ('after', 'IN'), ('them', 'PRP'), ('but', 'CC'), ('they', 'PRP'), ('flew', 'VBD'), ('away', 'RB'), ('to', 'TO'), ('their', 'PRP$'), ('friend', 'NN'), ("'s", 'POS'), ('household', 'NN'), ('then', 'RB'), ('the', 'DT'), ('mouse', 'NN'), ('cuts', 'VBZ'), ('the', 'DT'), ('net', 'JJ'), ('and', 'CC'), ('freed', 'VBD'), ('the', 'DT'), ('pigeons', 'NNS')]
 Transcribing /content/drive/MyDrive/Animals_Dataset/training/q87MwIW0.mp3
+Transcription: bear coming towards them
+Sentences: ['bear coming towards them']
+Linguistic analysis for sentence: bear coming towards them
+Word count: 4
+Sentence count: 1
+Part-of-speech tags: [('bear', 'IN'), ('coming', 'JJ'), ('towards', 'NNS'), ('them', 'PRP')]
 Transcribing /content/drive/MyDrive/Animals_Dataset/training/Vi8DU62C.mp3
+Transcription: the bear reaches near him and sniffs him in the ear after some time there left the place thinking the man is dead
+Sentences: ['the bear reaches near him and sniffs him in the ear after some time there left the place thinking the man is dead']
+Linguistic analysis for sentence: the bear reaches near him and sniffs him in the ear after some time there left the place thinking the man is dead
+Word count: 23
+Sentence count: 1
+Part-of-speech tags: [('the', 'DT'), ('bear', 'NN'), ('reaches', 'VBZ'), ('near', 'IN'), ('him', 'PRP'), ('and', 'CC'), ('sniffs', 'VBD'), ('him', 'PRP'), ('in', 'IN'), ('the', 'DT'), ('ear', 'NN'), ('after', 'IN'), ('some', 'DT'), ('time', 'NN'), ('there', 'EX'), ('left', 'VBD'), ('the', 'DT'), ('place', 'NN'), ('thinking', 'VBG'), ('the', 'DT'), ('man', 'NN'), ('is', 'VBZ'), ('dead', 'JJ')]
 Transcribing /content/drive/MyDrive/Animals_Dataset/training/1TEWgS4j.mp3
-Found 101 unique tokens.
-Model: "sequential_8"
-_________________________________________________________________
- Layer (type)                Output Shape              Param #   
-=================================================================
- embedding_8 (Embedding)     (None, 100, 100)          1000000   
-                                                                 
- bidirectional_8 (Bidirecti  (None, 256)               234496    
- onal)                                                           
-                                                                 
- dropout_8 (Dropout)         (None, 256)               0         
-                                                                 
- dense_8 (Dense)             (None, 11)                2827      
-                                                                 
-=================================================================
-Total params: 1237323 (4.72 MB)
-Trainable params: 1237323 (4.72 MB)
-Non-trainable params: 0 (0.00 Byte)
-_________________________________________________________________
-Epoch 1/10
-1/1 [==============================] - 6s 6s/step - loss: 2.3922 - accuracy: 0.0000e+00 - val_loss: 2.4026 - val_accuracy: 0.0000e+00
-Epoch 2/10
-1/1 [==============================] - 0s 370ms/step - loss: 2.3920 - accuracy: 0.1111 - val_loss: 2.4258 - val_accuracy: 0.0000e+00
-Epoch 3/10
-1/1 [==============================] - 0s 301ms/step - loss: 2.3695 - accuracy: 0.4444 - val_loss: 2.4506 - val_accuracy: 0.0000e+00
-Epoch 4/10
-1/1 [==============================] - 0s 347ms/step - loss: 2.3538 - accuracy: 0.4444 - val_loss: 2.4816 - val_accuracy: 0.0000e+00
-Epoch 5/10
-1/1 [==============================] - 0s 328ms/step - loss: 2.3555 - accuracy: 0.1111 - val_loss: 2.5103 - val_accuracy: 0.0000e+00
-Epoch 6/10
-1/1 [==============================] - 0s 326ms/step - loss: 2.3526 - accuracy: 0.1111 - val_loss: 2.5417 - val_accuracy: 0.0000e+00
-Epoch 7/10
-1/1 [==============================] - 0s 355ms/step - loss: 2.3139 - accuracy: 0.3333 - val_loss: 2.5764 - val_accuracy: 0.0000e+00
-Epoch 8/10
-1/1 [==============================] - 0s 333ms/step - loss: 2.2916 - accuracy: 0.6667 - val_loss: 2.6184 - val_accuracy: 0.0000e+00
-Epoch 9/10
-1/1 [==============================] - 0s 315ms/step - loss: 2.2614 - accuracy: 0.4444 - val_loss: 2.6722 - val_accuracy: 0.0000e+00
-Epoch 10/10
-1/1 [==============================] - 0s 322ms/step - loss: 2.2886 - accuracy: 0.1111 - val_loss: 2.7310 - val_accuracy: 0.0000e+00
+Transcription: fake friends
+Sentences: ['fake friends']
+Linguistic analysis for sentence: fake friends
+Word count: 2
+Sentence count: 1
+Part-of-speech tags: [('fake', 'VB'), ('friends', 'NNS')]
